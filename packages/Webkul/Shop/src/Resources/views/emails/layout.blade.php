e2ecfa521731cd18b6625492d7d6a45755d0fb42
smile_00112 <ooo.smile2000@yandex.ru>
2025-12-05 19:14:23 +0300

    fix mail

--- packages/Webkul/Shop/src/Resources/views/emails/layout.blade.php
+++ packages/Webkul/Shop/src/Resources/views/emails/layout.blade.php
@@ -27,23 +27,23 @@
             <div style="padding: 30px;">
                 <!-- Email Header -->
                 <div style="margin-bottom: 65px;">
-                    <a href="{{ route('shop.home.index') }}">
-                        @if ($logo = core()->getCurrentChannel()->logo_url)
-                            <img
-                                src="{{ $logo }}"
-                                alt="{{ config('app.name') }}"
-                                style="height: 40px; width: 110px;"
-                            />
-                        @else
-                            <img
-                                src="{{ bagisto_asset('images/logo.svg', 'shop') }}"
-                                alt="{{ config('app.name') }}"
-                                width="131"
-                                height="29"
-                                style="width: 156px;height: 40px;"
-                            />
-                        @endif
-                    </a>
+{{--                    <a href="{{ route('shop.home.index') }}">--}}
+{{--                        @if ($logo = core()->getCurrentChannel()->logo_url)--}}
+{{--                            <img--}}
+{{--                                src="{{ $logo }}"--}}
+{{--                                alt="{{ config('app.name') }}"--}}
+{{--                                style="height: 40px; width: 110px;"--}}
+{{--                            />--}}
+{{--                        @else--}}
+{{--                            <img--}}
+{{--                                src="{{ bagisto_asset('images/logo.svg', 'shop') }}"--}}
+{{--                                alt="{{ config('app.name') }}"--}}
+{{--                                width="131"--}}
+{{--                                height="29"--}}
+{{--                                style="width: 156px;height: 40px;"--}}
+{{--                            />--}}
+{{--                        @endif--}}
+{{--                    </a>--}}
                 </div>
 
                 <!-- Email Content -->
