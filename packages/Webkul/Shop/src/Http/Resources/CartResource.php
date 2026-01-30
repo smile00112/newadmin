24e2830eb26790dda153dec02a0fc52651125d0a
smile_00112 <ooo.smile2000@yandex.ru>
2026-01-30 13:55:48 +0300

    return  bonus_amount response field

--- packages/Webkul/Shop/src/Http/Resources/CartResource.php
+++ packages/Webkul/Shop/src/Http/Resources/CartResource.php
@@ -49,6 +49,10 @@ class CartResource extends JsonResource
             'payment_method'                     => $this->payment?->method,
             'payment_method_title'               => core()->getConfigData('sales.payment_methods.'.$this->payment?->method.'.title'),
             'auto_apply'                         => $this->auto_apply ?? false,
+            'bonus_amount'                       => $this->bonus_amount ?? 0,
+            'formatted_bonus_amount'             => core()->formatPrice($this->bonus_amount ?? 0),
+            'base_bonus_amount'                  => $this->base_bonus_amount ?? 0,
+            'formatted_base_bonus_amount'        => core()->formatBasePrice($this->base_bonus_amount ?? 0),
         ];
     }
 }
