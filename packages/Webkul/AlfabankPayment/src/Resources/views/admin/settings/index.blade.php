34e10b1aa43865e619dd4861ff2fbf4cdc0f7e56
smile_00112 <ooo.smile2000@yandex.ru>
2026-02-14 16:22:29 +0300

    fix

--- packages/Webkul/AlfabankPayment/src/Resources/views/admin/settings/index.blade.php
+++ packages/Webkul/AlfabankPayment/src/Resources/views/admin/settings/index.blade.php
@@ -87,6 +87,7 @@
                                 name="password"
                                 v-model="password"
                                 :label="'Пароль'"
+                                autocomplete="new-password"
                             />
                             <x-admin::form.control-group.error control-name="password" />
                         </x-admin::form.control-group>
@@ -100,6 +101,7 @@
                                 name="token"
                                 v-model="token"
                                 :label="'Токен'"
+                                autocomplete="new-password"
                             />
                             <x-admin::form.control-group.error control-name="token" />
                         </x-admin::form.control-group>
@@ -396,13 +398,36 @@
                     </div>
 
                     <div class="mt-6 flex items-center justify-end gap-x-2.5">
-                        <x-admin::button
-                            button-type="button"
+                        <button
+                            type="submit"
                             class="primary-button"
-                            title="Сохранить"
-                            ::loading="isLoading"
-                            ::disabled="isLoading"
-                        />
+                            :disabled="isLoading"
+                        >
+                            <span v-if="isLoading" class="flex items-center gap-2">
+                                <svg
+                                    class="h-5 w-5 animate-spin"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <circle
+                                        class="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        stroke-width="4"
+                                    ></circle>
+                                    <path
+                                        class="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                    ></path>
+                                </svg>
+                                Сохранение...
+                            </span>
+                            <span v-else>Сохранить</span>
+                        </button>
                     </div>
                 </div>
             </form>
