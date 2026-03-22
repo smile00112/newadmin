d48b3afd9fc7dc1d4cc862b01a3ef9c23dd57ef0
smile_00112 <ooo.smile2000@yandex.ru>
2026-03-22 19:13:05 +0300

    Fix rewerb test, add private chanel auth

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,16 @@ use App\Http\Controllers\OrderStatusCronController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/api-test', [ApiTestController::class, 'index'])->name('api.test');
+
+Route::get('/test/reverb', function () {
+    $path = base_path('test/reverb/index.html');
+
+    abort_unless(is_file($path), 404);
+
+    return response()->file($path, [
+        'Content-Type' => 'text/html; charset=UTF-8',
+    ]);
+})->name('test.reverb');
 Route::get('/cron/orders/pending-to-preparing', [OrderStatusCronController::class, 'pendingToPreparing'])
     ->name('cron.orders.pending-to-preparing');
 Route::get('/cron/orders/preparing-to-ready', [OrderStatusCronController::class, 'preparingToReady'])
